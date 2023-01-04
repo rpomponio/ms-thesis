a5563d16e00b84ec4bd92f025c348fb06ad1685c
rpomponio <raymond.pomponio@cuanschutz.edu>
2023-01-04 08:48:15 -0700

    initialized R project and first script

---
+++ Code/simulate.R
@@ -0,0 +1,6 @@
+################################################### -
+## Title: Script to run simulation
+## Author: Ray Pomponio
+## Email: raymond.pomponio@cuanschutz.edu
+## Date Created: 2023-01-04
+################################################### -
