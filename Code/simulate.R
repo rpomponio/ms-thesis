aa4b2f6b49450ae466fae38e1f163272e299f5e3
rpomponio <raymond.pomponio@cuanschutz.edu>
2023-01-12 13:50:31 -0500

    added ordinal distribution to simulation scenarios

--- Code/simulate.R
+++ Code/simulate.R
@@ -13,11 +13,11 @@ library(here)
 source(here("Code/estimators.R"))
 
 # HARDCODED PARAMETERS
-DISTRIB <- c("Normal")
+DISTRIB <- c("Continuous", "Ordinal")
 RHO <- c(-0.9, -0.5, -0.25, 0, 0.25, 0.5, 0.9)
-DELTA <- c(0) ###c(0, 0.25, 0.5)
-N <- c(10) ###c(10, 20, 50, 100, 200)
-PROP.MATCHED <- c(0.4, 0.8, 1) ###c(0, 0.05, 0.1, 0.15, 0.2, 0.3, 0.5)
+DELTA <- c(0, 0.25, 0.5)
+N <- c(10, 20, 50, 100, 200)
+PROP.MATCHED <- c(0, 0.05, 0.1, 0.15, 0.2, 0.3, 0.5, 1)
 SIGMA.X <- c(1)
 SIGMA.Y <- c(1)
 REP <- 1:10000
@@ -53,9 +53,23 @@ results <- foreach(i=1:n_datasets, .combine=rbind) %dorng%{
   X <- sim_mat[, 1]
   Y <- sim_mat[, 2]
   
+  # if ordinal: cut to integers from 1 to 7
+  if (params$Distribution=="Ordinal") {
+    cut_seq <- c(-999, seq(-2, 2, length.out=6), 999)
+    X <- as.numeric(cut(X, cut_seq))
+    Y <- as.numeric(cut(Y, cut_seq))
+  }
+  
   # setup
   n_matched <- floor(params$Prop.matched * params$N)
-  # boot_res <- cor.boot(X, Y, n_matched)
+  boot_res <- cor.boot(X, Y, n_matched)
+  
+  # MLEs of mu and sigma found using X, Y separately
+  mu_X <- sum(X) / params$N
+  mu_Y <- sum(Y) / params$N
+  sigma_sqd_X <- sum((X - mu_X)^2) / params$N
+  sigma_sqd_Y <- sum((Y - mu_Y)^2) / params$N
+  rho_hat <- cor(X, Y)
   
   # estimate correlation using various methods and aggregate results
   list(
@@ -68,17 +82,24 @@ results <- foreach(i=1:n_datasets, .combine=rbind) %dorng%{
     "N"=params$N,
     "M"=n_matched,
     "Prop.matched"=params$Prop.matched,
+    "Mu.hat.X"=mu_X,
+    "Mu.hat.Y"=mu_Y,
+    "Sigma.sqd.hat.X"=sigma_sqd_X,
+    "Sigma.sqd.hat.Y"=sigma_sqd_Y,
+    "Rho.hat"=rho_hat,
     # include all estimators below this line:
     "Max.conserv"        = est.cor.conserv(X, Y),
-    "Matched"            = est.cor.matched(X, Y, n_matched),
+    "Pearson"            = est.cor.pearson(X, Y, n_matched),
     "Shrunken"           = est.cor.shrunken(X, Y, n_matched),
     "Unbiased"           = est.cor.unbiased(X, Y, n_matched),
     "Freq.20th.quantile" = est.cor.quantile(X, Y, n_matched, q=0.2),
-    # "Boot.mean"          = boot_res$mean,
-    # "Boot.5th.quantile"  = boot_res$q.05,
-    # "Boot.20th.quantile" = boot_res$q.20,
+    "Boot.mean"          = boot_res$mean,
+    "Boot.5th.quantile"  = boot_res$q.05,
+    "Boot.20th.quantile" = boot_res$q.20,
     "EM.alg"             = est.cor.emalg(X, Y, n_matched),
-    "Bayes.unif"         = est.cor.bayesian.unif(X, Y, n_matched)
+    "Bayes.unif"         = est.cor.bayesian.unif(X, Y, n_matched),
+    "Bayes.Jeffreys"     = est.cor.bayesian.jeff(X, Y, n_matched),
+    "Bayes.Arcsine"      = est.cor.bayesian.asin(X, Y, n_matched)
     )
 }
 time_elapsed <- proc.time() - time_start
