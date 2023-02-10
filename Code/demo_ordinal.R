f32e057395c23aa10cc7e09fed463d19b7e2251b
rpomponio <raymond.pomponio@cuanschutz.edu>
2023-02-10 14:54:51 -0700

    updated description of simulation study

--- Code/demo_ordinal.R
+++ Code/demo_ordinal.R
@@ -7,12 +7,15 @@ set.seed(2023)
 
 N.ITER <- 1000
 
-rho_init <- seq(-0.9, 0.9, 0.1)
+rho_init <- seq(-0.9, 0.9, 0.05)
 eff_rho_avg <- rep(NA, length(rho_init))
+eff_var_avg <- rep(NA, 2)
 
 for (rho in rho_init) {
 
   rho_temp <- rep(NA, N.ITER)
+  var_x_temp <- rep(NA, N.ITER)
+  var_y_temp <- rep(NA, N.ITER)
   
   params <- list(Sigma.X=1, Sigma.Y=1, Rho=rho, N=1000, Delta=0,
                  Prop.matched=1.0, Distribution="Ordinal")
@@ -39,9 +42,14 @@ for (rho in rho_init) {
     }
 
     rho_temp[i] <- cor(X, Y)
+    
+    var_x_temp[i] <- var(X)
+    var_y_temp[i] <- var(Y)
   }
 
   eff_rho_avg[which(rho_init==rho)] <- mean(rho_temp)
+  eff_var_avg[1] <- mean(var_x_temp[i])
+  eff_var_avg[2] <- mean(var_y_temp[i])
 }
 
 
@@ -49,3 +57,13 @@ plot(rho_init, eff_rho_avg, type='l', xlim=c(-1, 1), ylim=c(-1, 1))
 lines(rho_init, rho_init, col='blue')
 title('Initial values of rho versus "effective correlation"')
 
+
+eff_rho_avg[rho_init==-0.9]
+eff_rho_avg[rho_init==-0.5]
+eff_rho_avg[rho_init==-0.25]
+eff_rho_avg[rho_init==0]
+eff_rho_avg[round(rho_init, 2)==0.25]
+eff_rho_avg[round(rho_init, 2)==0.5]
+eff_rho_avg[round(rho_init, 2)==0.9]
+
+eff_var_avg
