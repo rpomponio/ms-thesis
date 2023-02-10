49802920e28e038f4349c43d9a7ec660d356d335
rpomponio <raymond.pomponio@cuanschutz.edu>
2023-02-10 16:25:07 -0700

    refactored processing to separate script, fixed boundary issues

--- Code/analyze.R
+++ Code/analyze.R
@@ -8,104 +8,22 @@
 options(stringsAsFactors=FALSE)
 library(dplyr, warn.conflicts=FALSE)
 options(dplyr.summarise.inform=FALSE)
-library(tidyr)
 library(ggplot2)
 theme_set(ggpubr::theme_classic2(base_size=12))
 library(here)
 
-# load previously-saved simulation results
-results <- readRDS(here("DataRaw/simulation_results_2023-02-03.rds"))
-
-# temporary fix for NaNs and Infs (occurs in shrunken estimator)
-results[!is.finite(results)] <- NA
-
-# pivot to long data frame
-df_results_long <- data.frame(results) %>%
-  pivot_longer(cols=Max.conserv:Bayes.arcsine, names_to="Method") %>%
-  rename(Estimate=value)
-
-# when are estimates outside bounds of (-1, 1)?
-# df_results_long %>%
-#   filter(Estimate > 1 | Estimate < -1) %>%
-#   sample_n(100) %>%
-#   View()
-
-# note boundary occurs for Pearson when m=2 (technically valid)
-# df_results_long %>%
-#   filter(Distribution==1, Delta==0, Rho==0.5, Method=="Pearson", N==10, M==2) %>%
-#   filter(Estimate == 1 | Estimate == -1) %>%
-#   View()
-
-# flag all "Failures" as cases where valid correlation was not estimated
-df_results_long <- df_results_long %>%
-  mutate(Failure.type=case_when(
-    !is.finite(Estimate) ~ "Undefined",
-    Estimate < -1 | Estimate > 1 ~ "Invalid",
-    abs(Estimate - 1) < 0.000001 ~ "Boundary",
-    abs(Estimate + 1) < 0.000001 ~ "Boundary",
-    TRUE ~ "Nonfailure")) %>%
-  mutate(Failure=ifelse(Failure.type=="Nonfailure", 0, 1))
-
-# calculate failure rate per method
-df_failures <- df_results_long %>%
-  group_by(Method, Distribution, Rho, Delta, N, Prop.matched, M) %>%
-  summarise(
-    Num.iterations = n(),
-    Num.failures=sum(Failure),
-    Failure.rate=mean(Failure)) %>%
-  ungroup()
-
-# when are failures occurring most frequently (with sample size > 3)?
-# df_failures %>%
-#   filter(Failure.rate > 0.01, M > 3) %>%
-#   View()
-
-# oracle method: use true correlation
-df_oracle <- df_results_long %>%
-  filter(Method=="Max.conserv") %>%
-  mutate(Estimate=Rho, Method="Oracle", Falure=0)
-
-# naive method: assume zero correlation
-df_naive <- df_results_long %>%
-  filter(Method=="Max.conserv") %>%
-  mutate(Estimate=0, Method="Independence", Failure=0)
-
-# compute (modified) t-statistics and corresponding p-values
-df_inference <- df_results_long %>%
-  bind_rows(df_oracle, df_naive) %>%
-  mutate(
-    d.bar = Mu.hat.X - Mu.hat.Y,
-    sum.sqs = (Sigma.sqd.hat.X * N) + (Sigma.sqd.hat.Y * N),
-    t.ind = d.bar / sqrt( sum.sqs / (N * (N - 1)) ),
-    t.mod = suppressWarnings(ifelse(Failure==1, NA, t.ind / sqrt(1 - Estimate))),
-    p.value = 2 * pt(abs(t.mod), df=(2 * N - 2), lower.tail=F),
-    d.lwr = 0, # @Ryan: how to compute Conf Int of mean for modified test?
-    d.upr = 0) %>%
-  group_by(Method, Distribution, Rho, Delta, N, Prop.matched, M) %>%
-  summarise(Rejection.rate=mean(p.value < 0.05, na.rm=T)) %>%
-  ungroup()
-
-# create "error" matrix
-errors <- results[, 15:26] - results[, "Rho"]
-
-# calculate average error, or "bias", and mean squared error
-df_performance <- data.frame(errors) %>%
-  bind_cols(select(data.frame(results), Distribution:Prop.matched)) %>%
-  pivot_longer(cols=Max.conserv:Bayes.arcsine, names_to="Method") %>%
-  group_by(Method, Distribution, Rho, Delta, N, Prop.matched, M) %>%
-  summarise(Bias=mean(value, na.rm=T), Mean.sqd.error=mean(value^2, na.rm=T)) %>%
-  ungroup()
-
-# save "processed" data
-saveRDS(df_failures, here("DataProcessed/failures_2023-02-03.rds"))
-saveRDS(df_inference, here("DataProcessed/inference_2023-02-03.rds"))
-saveRDS(df_performance, here("DataProcessed/performance_2023-02-03.rds"))
+# load processed data
+df_results_long <- readRDS(here("DataProcessed/all_2023-02-03.rds"))
+df_failures <- readRDS(here("DataProcessed/failures_2023-02-03.rds"))
+df_inference <- readRDS(here("DataProcessed/inference_2023-02-03.rds"))
+df_performance <- readRDS(here("DataProcessed/performance_2023-02-03.rds"))
     
 # plot bias as a function of true correlation
 df_performance %>%
+  filter(!(Method=="Max.conserv" & Bias < -1) & !(Method=="EM.alg" & M==0)) %>%
   filter(Distribution==1, Delta==0, N==10,
-         Method %in% c("EM.alg", "Bayes.arcsine", "Pearson",
-                       "Shrunken", "Unbiased", "Freq.20th.quantile"),
+         Method %in% c("Rho.hat", "EM.alg", "Bayes.Jeffreys", "Pearson",
+                       "Freq.20th.quantile", "Max.conserv"),
          Prop.matched %in% c(0, 0.1, 0.2, 0.3, 0.5, 1)) %>%
   ggplot(aes(col=Method, x=Rho, y=Bias)) +
   geom_hline(yintercept=0, linetype="dashed") +
@@ -113,52 +31,52 @@ df_performance %>%
   geom_line() +
   geom_point() +
   scale_x_continuous(breaks=unique(df_performance$Rho)) +
-  labs(title="Bias of estimators by number of matched samples (0 to 10)",
-       subtitle="Measured by mean difference (in all cases total sample size was 10)",
+  labs(title="Bias of estimators by number of matched samples",
+       subtitle="Varied from 0 to 10 matched samples (n=10)",
        x="True correlation",
        y="Bias (Estimated minus true value)",
        caption="Results averaged over 1,000 datasets at each point.")
 
-ggsave(filename="~/Downloads/Sim_Results_Bias.png", width=10.5, height=7.5)
-
 # plot MSE as a function of true correlation
 df_performance %>%
+  filter(!(Method=="Max.conserv" & Mean.sqd.error > 1) & !(Method=="EM.alg" & M==0)) %>%
   filter(Distribution==1, Delta==0, N==10,
-         Method %in% c("EM.alg", "Bayes.arcsine", "Pearson",
-                       "Shrunken", "Unbiased", "Freq.20th.quantile"),
+         Method %in% c("Rho.hat", "EM.alg", "Bayes.Jeffreys", "Pearson",
+                       "Freq.20th.quantile", "Max.conserv"),
          Prop.matched %in% c(0, 0.1, 0.2, 0.3, 0.5, 1)) %>%
   ggplot(aes(col=Method, x=Rho, y=Mean.sqd.error)) +
   facet_wrap(~ M, scales="free_y") +
   geom_line() +
   geom_point() +
   scale_x_continuous(breaks=unique(df_performance$Rho)) +
-  labs(title="Variance of estimators by number of matched samples (0 to 10)",
-       subtitle="Measured by Mean Sqd. Error (in all cases total sample size was 10)",
+  labs(title="MSE of estimators by number of matched samples",
+       subtitle="Varied from 0 to 10 matched samples (n=10)",
        x="True correlation",
        caption="Results averaged over 1,000 datasets at each point.")
 
-ggsave(filename="~/Downloads/Sim_Results_Variance.png", width=10.5, height=7.5)
-
-# plot correlation with correct MLE estimator under fully matched samples
+# plot association with between any two estimators
 df_results_long %>%
-  filter(Distribution==1, Delta==0, N==20,
-         Method %in% c("EM.alg", "Bayes.arcsine", "Pearson",
-                       "Shrunken", "Unbiased", "Freq.20th.quantile"),
-         M == 10) %>%
+  filter(!(Method=="EM.alg" & M==0)) %>%
+  filter(Distribution==1, Delta==0, N==10,
+         Method %in% c("Rho.hat", "EM.alg"),
+         Prop.matched %in% c(0, 0.1, 0.2, 0.3, 0.5, 1)) %>%
+  tidyr::pivot_wider(names_from=Method, values_from=Estimate) %>%
   sample_frac(0.1) %>%
-  ggplot(aes(x=Rho.hat, y=Estimate)) +
-  facet_wrap(~ Method) +
-  geom_point() +
-  geom_abline(intercept=0, slope=1, linetype="dashed", col="blue") +
-  labs(title="Correlation between estimators and 'correct' estimator",
-       x="Pearson correlation of matched samples",
+  ggplot(aes(x=Rho.hat, y=EM.alg)) +
+  facet_wrap(~ M) +
+  geom_point(alpha=0.25) +
+  geom_smooth(method="lm", formula="y ~ x", se=F) +
+  geom_abline(intercept=0, slope=1, linetype="dashed") +
+  labs(title="Scatterplot of association between estimators",
+       subtitle="Varied from 0 to 10 matched samples (n=10)",
        caption="Results sampled from 1,000 datasets for efficiency.")
 
 # plot Type-I error rate as function of correlation
 df_inference %>%
+  filter(!(Method=="EM.alg" & M==0)) %>%
   filter(Distribution==1, Delta==0, N==10,
-         Method %in% c("EM.alg", "Bayes.arcsine", "Pearson",
-                       "Shrunken", "Oracle", "Independence"),
+         Method %in% c("Oracle", "Independent", "Pearson",
+                       "EM.alg", "Bayes.Jeffreys"),
          Prop.matched %in% c(0, 0.1, 0.2, 0.3, 0.5, 1)) %>%
   ggplot(aes(col=Method, x=Rho, y=Rejection.rate)) +
   geom_hline(yintercept=0.05, linetype="dashed") +
@@ -166,8 +84,8 @@ df_inference %>%
   geom_line() +
   geom_point() +
   scale_x_continuous(breaks=unique(df_inference$Rho)) +
-  labs(title="Type-I error of methods by number of matched samples (0 to 10)",
-       subtitle="In all cases total sample size was 10",
+  labs(title="Type-I error rates by number of matched samples",
+       subtitle="Varied from 0 to 10 matched samples (n=10)",
        x="True correlation",
        y="Type-I error (rejected null hypotheses)",
        caption="Results averaged over 1,000 datasets at each point.")
---
+++ Code/process.R
@@ -0,0 +1,107 @@
+################################################### -
+## Title: Process simulation results for down-stream analysis
+## Author: Ray Pomponio
+## Email: raymond.pomponio@cuanschutz.edu
+## Date Created: 2023-02-10
+################################################### -
+
+options(stringsAsFactors=FALSE)
+library(dplyr, warn.conflicts=FALSE)
+options(dplyr.summarise.inform=FALSE)
+library(tidyr)
+library(here)
+
+# load previously-saved simulation results
+results <- readRDS(here("DataRaw/simulation_results_2023-02-03.rds"))
+
+# temporary fix for NaNs and Infs (occurs in shrunken estimator)
+results[!is.finite(results)] <- NA
+
+# pivot to long data frame
+df_results_long <- data.frame(results) %>%
+  pivot_longer(cols=Rho.hat:Bayes.arcsine, names_to="Method") %>%
+  rename(Estimate=value)
+
+# warn of invalid estimates
+n_ests_invalid <- sum(df_results_long$Estimate > 1 | df_results_long$Estimate < -1, na.rm=T)
+warning(paste0("Number of invalid correlation estimates: ", n_ests_invalid))
+
+# flag all "Failures" as cases where valid correlation was not estimated
+df_results_long <- df_results_long %>%
+  mutate(Failure.type=case_when(
+    !is.finite(Estimate) ~ "Undefined",
+    Estimate < -1 | Estimate > 1 ~ "Invalid",
+    abs(Estimate - 1) < 0.000001 ~ "Boundary",
+    abs(Estimate + 1) < 0.000001 ~ "Boundary",
+    TRUE ~ "Nonfailure")) %>%
+  mutate(Failure=ifelse(Failure.type=="Nonfailure", 0, 1))
+
+# calculate failure rate per method
+df_failures <- df_results_long %>%
+  group_by(Method, Distribution, Rho, Delta, N, Prop.matched, M) %>%
+  summarise(
+    Num.iterations=n(),
+    Num.failures=sum(Failure),
+    Failure.rate=mean(Failure)) %>%
+  ungroup()
+
+# for 'oracle' inference method: use true correlation (or effective correlation)
+df_oracle <- df_results_long %>%
+  filter(Method=="Rho.hat") %>%
+  mutate(Estimate=case_when(
+    Distribution==1 ~ Rho,
+    Rho==-0.9  ~ -0.579453,
+    Rho==-0.5  ~ -0.3591659,
+    Rho==-0.25 ~ -0.1890325,
+    Rho==0     ~ 0.0009432011,
+    Rho==0.25  ~ 0.203865,
+    Rho==0.5   ~ 0.425501,
+    Rho==0.9   ~ 0.8407945)) %>%
+  mutate(Method="Oracle")
+
+# for naive inference method: assume zero correlation
+df_naive <- df_results_long %>%
+  filter(Method=="Rho.hat") %>%
+  mutate(Estimate=0) %>%
+  mutate(Method="Independent")
+
+# compute (modified) t-statistics and corresponding p-values
+df_inference <- df_results_long %>%
+  bind_rows(df_oracle, df_naive) %>%
+  mutate(Estimate=ifelse(Failure.type=="Boundary", Estimate * 0.9999, Estimate)) %>%
+  mutate(Failure=ifelse(Failure.type=="Boundary", 0, Failure)) %>%
+  mutate(
+    d.bar = Mu.hat.X - Mu.hat.Y,
+    sum.sqs = (Sigma.sqd.hat.X * N) + (Sigma.sqd.hat.Y * N),
+    t.ind = d.bar / sqrt( sum.sqs / (N * (N - 1)) ),
+    t.mod = suppressWarnings(ifelse(Failure==1, NA, t.ind / sqrt(1 - Estimate))),
+    p.value = 2 * pt(abs(t.mod), df=(2 * N - 2), lower.tail=F),
+    d.lwr = NA, # @Ryan: how to compute Conf Int of mean for modified test?
+    d.upr = NA) %>%
+  group_by(Method, Distribution, Rho, Delta, N, Prop.matched, M) %>%
+  summarise(Rejection.rate=mean(p.value < 0.05, na.rm=T)) %>%
+  ungroup()
+
+# calculate bias and MSE (accounting for effective correlation in ordinal data)
+df_performance <- df_results_long %>%
+  mutate(Error=case_when(
+    Distribution==1 ~ Estimate - Rho,
+    Rho==-0.9  ~ Estimate - (-0.579453),
+    Rho==-0.5  ~ Estimate - (-0.3591659),
+    Rho==-0.25 ~ Estimate - (-0.1890325),
+    Rho==0     ~ Estimate - (0.0009432011),
+    Rho==0.25  ~ Estimate - (0.203865),
+    Rho==0.5   ~ Estimate - (0.425501),
+    Rho==0.9   ~ Estimate - (0.8407945))) %>%
+  group_by(Method, Distribution, Rho, Delta, N, Prop.matched, M) %>%
+  summarise(Bias=mean(Error, na.rm=T), Mean.sqd.error=mean(Error^2, na.rm=T)) %>%
+  ungroup()
+
+# save processed data
+saveRDS(df_results_long, here("DataProcessed/all_2023-02-03.rds"))
+saveRDS(df_failures, here("DataProcessed/failures_2023-02-03.rds"))
+saveRDS(df_inference, here("DataProcessed/inference_2023-02-03.rds"))
+saveRDS(df_performance, here("DataProcessed/performance_2023-02-03.rds"))
+
+
+
