d0b99c33b93a60c9f78a917ba0ed9d9041d5e55d
rpomponio <raymond.pomponio@cuanschutz.edu>
2023-03-06 18:36:49 -0700

    created new plot of mBias and table for failure summary

--- Code/analyze.R
+++ Code/analyze.R
@@ -10,14 +10,71 @@ library(dplyr, warn.conflicts=FALSE)
 options(dplyr.summarise.inform=FALSE)
 library(ggplot2)
 theme_set(ggpubr::theme_classic2(base_size=12))
+library(kableExtra, warn.conflicts=FALSE)
 library(here)
 
 # load processed data
-df_results_long <- readRDS(here("DataProcessed/lite_2023-02-20.rds"))
-df_failures <- readRDS(here("DataProcessed/failures_2023-02-20.rds"))
-df_inference <- readRDS(here("DataProcessed/inference_2023-02-20.rds"))
-df_performance <- readRDS(here("DataProcessed/performance_2023-02-20.rds"))
-    
+# df_results_long <- readRDS(here("DataProcessed/all_2023-02-20.rds"))
+# df_failures <- readRDS(here("DataProcessed/failures_2023-02-20.rds"))
+# df_inference <- readRDS(here("DataProcessed/inference_2023-02-20.rds"))
+# df_performance <- readRDS(here("DataProcessed/performance_2023-02-20.rds"))
+
+##### Summarize failure rates by simulation setting #####
+
+tab_failures <- df_failures %>%
+  filter(Distribution==2,
+         Delta==0,
+         N <= 50,
+         Method %in% c("Bayes.arcsine", "EM.alg", "Freq.20th.quantile",
+                       "Pearson")) %>%
+  select(Method, Rho, N, Prop.matched, M, Failure.rate) %>%
+  tidyr::pivot_wider(id_cols=c(Method, N, Prop.matched, M),
+                     names_from=Rho,
+                     values_from=Failure.rate)
+
+tab_failures %>%
+  kable(digits=3) %>%
+  kable_styling(fixed_thead=T, full_width=F, font_size=16) %>%
+  column_spec(5, background=spec_color(tab_failures$`-0.9`, alpha=0.5)) %>%
+  column_spec(6, background=spec_color(tab_failures$`-0.5`, alpha=0.5)) %>%
+  column_spec(7, background=spec_color(tab_failures$`-0.25`, alpha=0.5)) %>%
+  column_spec(8, background=spec_color(tab_failures$`0`, alpha=0.5)) %>%
+  column_spec(9, background=spec_color(tab_failures$`0.25`, alpha=0.5)) %>%
+  column_spec(10, background=spec_color(tab_failures$`0.5`, alpha=0.5)) %>%
+  column_spec(11, background=spec_color(tab_failures$`0.9`, alpha=0.5))
+
+##### Plot bias as function of matched samples #####
+
+# Calculate simulated P(r > rho) for various values of m for each method
+# If r > rho, we'd expect SEs to be too low 
+df_results_long %>%
+  filter(Method %in% c("Bayes.arcsine", "EM.alg", "Freq.20th.quantile",
+                       "Pearson")) %>%
+  filter(Distribution==1, Delta==0, N==20,
+         Prop.matched %in% c(0, 0.1, 0.2, 0.3, 0.5, 1)) %>%
+  group_by(Method, Rho, Prop.matched, M) %>%
+  summarise(Prop.overestimate=mean(Estimate > Rho, na.rm=T)) %>%
+  filter(!is.na(Prop.overestimate)) %>%
+  ggplot(aes(col=Method, x=M, y=Prop.overestimate)) +
+  geom_hline(yintercept=0.5, linetype="dashed") +
+  facet_wrap(~ Rho) +
+  geom_line() +
+  geom_point(size=2) +
+  ylim(c(0, 1)) +
+  scale_x_continuous(breaks=seq(1, 20, 2)) +
+  labs(subtitle="Simulated with n=20, number of matched samples varying.",
+       x="Number of matched samples",
+       y="mBias",
+       caption="mBias is the probability P(r > Rho)")
+
+# ggsave("~/Downloads/mBias.png", width=10, height=8)
+
+
+
+
+
+##### Reproduce Shiny App plots #####
+
 # plot bias as a function of true correlation
 df_performance %>%
   filter(!(Method=="Max.conserv" & Bias < -1)) %>%
